ee305d3141558120a4db4d8df4385b3e994a75f1
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2019-08-22 21:03:14 +1000

    Adding hook displays to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import mouseIcon from './icons/mouseIcon.svg';
+import timeIcon from './icons/timeIcon.svg';
+import viewsIcon from './icons/viewsIcon.svg';
 import './App.css';
+import useMouseDistance from './hooks/useMouseDistance';
+import usePageVisits from './hooks/usePageVisits';
+import usePageMilliseconds from './hooks/usePageMilliseconds';
+
+const millisecondsToTime = (milliseconds: number) => new Date(milliseconds).toISOString().slice(11, -5);
 
 const App: React.FC = () => {
+  const [mouseDistance] = useMouseDistance();
+  const [pageVisits] = usePageVisits();
+  const [pageMilliseconds] = usePageMilliseconds();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={mouseIcon} alt="mouseIcon" />
+        <span>{mouseDistance}</span>
+        <span>pixels travelled</span>
+        <img src={timeIcon} alt="timeIcon" />
+        <span>{millisecondsToTime(pageMilliseconds)}</span>
+        <span>milliseconds</span>
+        <img src={viewsIcon} alt="viewsIcon" />
+        <span>{pageVisits}</span>
+        <span>views</span>
       </header>
     </div>
   );
