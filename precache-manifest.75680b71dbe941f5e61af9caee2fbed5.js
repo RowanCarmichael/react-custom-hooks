aeb32d571fd14ae4eab8c69e8dfc3c5753b9a1c5
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2019-08-25 11:33:09 +1000

    Updates

---
+++ precache-manifest.75680b71dbe941f5e61af9caee2fbed5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2fc25c2be3ddfff3684a084687edd3",
+    "url": "/react-custom-hooks/index.html"
+  },
+  {
+    "revision": "77db1dab703c7933f297",
+    "url": "/react-custom-hooks/static/css/main.9d4692f1.chunk.css"
+  },
+  {
+    "revision": "11a423282fb401c0ac88",
+    "url": "/react-custom-hooks/static/js/2.9289bfb4.chunk.js"
+  },
+  {
+    "revision": "77db1dab703c7933f297",
+    "url": "/react-custom-hooks/static/js/main.5789b7ce.chunk.js"
+  },
+  {
+    "revision": "a77a3deb4e3bbcec78dc",
+    "url": "/react-custom-hooks/static/js/runtime~main.a69e046a.js"
+  },
+  {
+    "revision": "44a553713fc09ddd9363934556f7c734",
+    "url": "/react-custom-hooks/static/media/mouseIcon.44a55371.svg"
+  },
+  {
+    "revision": "c81f4f8501e46beef8a789d910295566",
+    "url": "/react-custom-hooks/static/media/timeIcon.c81f4f85.svg"
+  },
+  {
+    "revision": "269c63a5f4d4151e28ce757080a00532",
+    "url": "/react-custom-hooks/static/media/viewsIcon.269c63a5.svg"
+  }
+]);
--- src/utils/millisecondsToTime.ts
+++
@@ -1,3 +0,0 @@
-const millisecondsToTime = (milliseconds: number) => new Date(milliseconds).toISOString().slice(11, -5);
-
-export default millisecondsToTime;
