441f28812b21863e8600db9475069bac5e4a993e
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2019-08-25 10:49:28 +1000

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# react-context-api-example
-
-A small project to learn how to create and use custom React hooks introduced in React v16.8
-
-## Live Demo (with code snippits)
-https://RowanCarmichael.github.io/react-custom-hooks
--- src/utils/getMouseDistance.ts
+++
@@ -1,4 +0,0 @@
-const getMouseDistance = (previousX: number, previousY: number, currentX: number, currentY: number) =>
-  Math.round(Math.sqrt(Math.pow(previousY - currentY, 2) + Math.pow(previousX - currentX, 2)));
-
-export default getMouseDistance;
